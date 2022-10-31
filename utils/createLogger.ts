390e08f41b58001d0f211d4004833a2efda9bcef
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-10-31 16:19:11 +0800

    fix (logging): do not use pino-pretty in prod

--- utils/createLogger.ts
+++ utils/createLogger.ts
@@ -3,8 +3,10 @@ import pino from 'pino';
 // Export a shared pino instance for use in server code
 const parentLogger = pino({
 //  level: process.env.NODE_ENV === 'development' ? 'trace' : 'info',
-  level: 'trace', // Using trace for now to debug issues in prod hosting
-  transport: {
+
+  // Using trace for now to debug issues in prod hosting
+  level: 'trace',
+  transport: process.env.NODE_ENV === 'production' ? undefined : {
     target: 'pino-pretty'
   }
 })
