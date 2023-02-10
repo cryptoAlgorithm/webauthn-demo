86bb8a47ff65a6a1baa0dbd8068dd163682b739f
kwkinet <kwkinet@gmail.com>
2023-02-10 22:11:57 +0800

    test (jest): add test and coverage reporting

---
+++ jest.coverage.js
@@ -0,0 +1,34 @@
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: [
+    'dotenv/config'
+  ],
+
+  reporters: [
+    'default',
+    ['jest-html-reporter', {
+      'pageTitle': 'Test Report'
+    }]
+  ],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!./coverage/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  // this setting is required for jest config using config files!
+  coverageProvider: 'v8',
+
+  coverageReporters: ['html'],
+};
+
+module.exports = config;
