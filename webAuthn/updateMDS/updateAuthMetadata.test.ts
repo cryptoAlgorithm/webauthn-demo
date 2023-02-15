c0246efaa32445fc5148fe5911c2de6d33cce555
kwkinet <kwkinet@gmail.com>
2023-02-15 16:55:45 +0800

    feat (tests): add verifyAttestionTrust test

--- webAuthn/updateMDS/updateAuthMetadata.test.ts
+++ webAuthn/updateMDS/updateAuthMetadata.test.ts
@@ -1,11 +1,21 @@
 import {describe, expect, test} from '@jest/globals'
 import {updateAuthMetadata} from "./updateAuthMetadata";
 import {AUTHN_WHITELIST} from "../mds/authWhiteList";
+import {verifyAttestationTrust} from "../mds/verifyAttestationTrust";
 
 describe('Validate MDS', () => {
   test('Update MDS to JWT', async () => {
       await expect(updateAuthMetadata()).resolves.toBe(AUTHN_WHITELIST.length)
     },
     300_000)
+
+    test('Verify Attestation Trust', async () => {
+        const testAttCert = 'MIICvTCCAaWgAwIBAgIEGKxGwDANBgkqhkiG9w0BAQsFADAuMSwwKgYDVQQDEyNZdWJpY28gVTJGIFJvb3QgQ0EgU2VyaWFsIDQ1NzIwMDYzMTAgFw0xNDA4MDEwMDAwMDBaGA8yMDUwMDkwNDAwMDAwMFowbjELMAkGA1UEBhMCU0UxEjAQBgNVBAoMCVl1YmljbyBBQjEiMCAGA1UECwwZQXV0aGVudGljY' +
+            'XRvciBBdHRlc3RhdGlvbjEnMCUGA1UEAwweWXViaWNvIFUyRiBFRSBTZXJpYWwgNDEzOTQzNDg4MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEeeo7LHxJcBBiIwzSP+tg5SkxcdSD8QC+hZ1rD4OXAwG1Rs3Ubs/K4+PzD4Hp7WK9Jo1MHr03s7y+kqjCrutOOqNsMGowIgYJKwYBBAGCxAoCBBUxLjMuNi4' +
+            'xLjQuMS40MTQ4Mi4xLjcwEwYLKwYBBAGC5RwCAQEEBAMCBSAwIQYLKwYBBAGC5RwBAQQEEgQQy2lIHo/3QDmT7AonKaFUqDAMBgNVHRMBAf8EAjAAMA0GCSqGSIb3DQEBCwUAA4IBAQCXnQOX2GD4LuFdMRx5brr7Ivqn4ITZurTGG7tX8+a0wYpIN7hcPE7b5IND9Nal2bHO2orh/tSRKSFzBY5e4cvda9rAd' +
+            'VfGoOjTaCW6FZ5/ta2M2vgEhoz5Do8fiuoXwBa1XCp61JfIlPtx11PXm5pIS2w3bXI7mY0uHUMGvxAzta74zKXLslaLaSQibSKjWKt9h+SsXy4JGqcVefOlaQlJfXL1Tga6wcO0QTu6Xq+Uw7ZPNPnrpBrLauKDd202RlN4SP7ohL3d9bG6V5hUz/3OusNEBZUn5W3VmPj1ZnFavkMB3RkRMOa58MZAORJT4imAPzrvJ0vtv94/y71C6tZ5'
+        const testAaguid = 'cb69481e-8ff7-4039-93ec-0a2729a154a8'
+        await verifyAttestationTrust(testAaguid, [testAttCert])
+    })
 })
 
